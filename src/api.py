89554d2bd8ac51f50086d80ec0b44ddf878ea1d0
msm <msm@cert.pl>
2019-12-09 13:52:56 +0100

    Fix erorrs

--- src/api.py
+++ src/api.py
@@ -4,10 +4,17 @@ from typing import Dict, Any
 import logging
 
 
-def parse(family: str, config: Dict[str, Any]) -> IocCollection:
+class FamilyNotSupportedYetError(RuntimeError):
+    pass
+
+
+def parse(
+    family: str, config: Dict[str, Any], raise_on_not_supported: bool = False
+) -> IocCollection:
     if family not in modules:
         logging.warning(f"Family %s is not supported by iocextract", family)
-        raise RuntimeError()
+        if raise_on_not_supported:
+            raise FamilyNotSupportedYetError()
         return IocCollection()
 
     return modules[family](config)
