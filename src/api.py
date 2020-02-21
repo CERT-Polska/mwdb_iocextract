1ae8b6d6bc588484ec1dcf413a7d8c1efcc46dfb
msm <msm@tailcall.net>
2020-02-21 17:05:44 +0100

    Fix flake8 problems

--- src/api.py
+++ src/api.py
@@ -1,8 +1,7 @@
 from .modules import modules
 from .model import IocCollection
 from typing import Dict, Any, Optional
-import logging
-from .errors import FamilyNotSupportedYetError, IocExtractError
+from .errors import FamilyNotSupportedYetError
 
 
 def parse(family: str, config: Dict[str, Any]) -> Optional[IocCollection]:
