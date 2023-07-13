9f5b0b657f6136ac649abbcb134df294f7790f00
Michał Praszmo <michalpr@cert.pl>
2023-07-13 15:13:37 +0200

    Remove debug data

--- tests/test_parse_regression.py
+++ tests/test_parse_regression.py
@@ -26,13 +26,6 @@ class TestParseRegression(unittest.TestCase):
             with open(testdir + expected_file_txt, "rb") as exp:
                 expected_data = exp.read().decode("utf-8")
 
-            with open("/tmp/expected", "w") as f:
-                f.write(expected_data)
-            
-            with open("/tmp/got", "w") as f:
-                f.write(iocs.prettyprint() + "\n")
-                
-
             if expected_data != iocs.prettyprint() + "\n":
                 print("EXPECTED")
                 print(expected_data)
