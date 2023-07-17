c2ef904efcf5f11429df2fa2dd04e3e76e547e9f
msm <msm@tailcall.net>
2023-07-17 14:32:45 +0200

    Update

--- setup.py
+++ setup.py
@@ -3,13 +3,14 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open("src/requirements.txt", "r") as fh:
+with open("requirements.txt", "r") as fh:
     requirements = fh.read().split('\n')
 
 setuptools.setup(
     name="mwdb-iocextract",
     version="1.1.4",
     author="msm",
+    data_files=[('requirements.txt', ['requirements.txt'])],
     author_email="msm@cert.pl",
     package_dir={"mwdb_iocextract": "src"},
     packages=["mwdb_iocextract"],
